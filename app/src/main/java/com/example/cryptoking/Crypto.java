527b70169fc2e9db0f5e71c4e2f6ef7b0062cb35
Aditya <adityadeshmukh7350@gmail.com>
2021-09-25 10:17:31 -0700

    Added java files

--- app/src/main/java/com/example/cryptoking/Crypto.java
+++ app/src/main/java/com/example/cryptoking/Crypto.java
@@ -1,4 +1,47 @@
 package com.example.cryptoking;
 
-public class Crypto {
+import android.app.AlertDialog;
+import android.app.Application;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.res.Configuration;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class Crypto extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Configuration  configuration = new Configuration();
+        configuration.locale = Locale.ENGLISH;
+        super.onConfigurationChanged(configuration);
+        Locale.setDefault(configuration.locale);
+        getBaseContext().getResources().updateConfiguration(configuration,getResources().getDisplayMetrics());
+    }
+
+    public static void Loger(String m){
+        Log.e("mip tag",m);
+
+    }
+    public static void Tostmaker(Context c,String m){
+        Toast.makeText(c,m,Toast.LENGTH_SHORT).show();
+
+    }
+    public static void Dialogmaker(Context c,String title,String mes){
+        AlertDialog.Builder alert = new AlertDialog.Builder(c);
+        alert.setCancelable(true);
+        alert.setTitle(title);
+        alert.setMessage(mes);
+        alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        alert.show();
+
+    }
 }
